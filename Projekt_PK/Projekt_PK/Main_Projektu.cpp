6bd66e12093e392ddc59f2a0d21dc42d04f64a6e
MichalWitczakmw <mw305423@student.polsl.pl>
2024-12-30 20:32:23 +0100

    Testowy comit

---
+++ Projekt_PK/Projekt_PK/Main_Projektu.cpp
@@ -0,0 +1,54 @@
+#include "Main_Projektu.h"
+
+class Generator
+{
+public:
+	Generator(){}
+	~Generator(){}
+
+private:
+
+};
+
+class Sprzerzenie
+{
+public:
+	Sprzerzenie() {}
+	~Sprzerzenie() {}
+
+private:
+
+};
+class Regulator
+{
+public:
+	Regulator() {}
+	~Regulator() {}
+
+private:
+
+};
+class ModelUAR
+{
+public:
+	ModelUAR() {}
+	~ModelUAR() {}
+
+private:
+
+};
+class Generator
+{
+public:
+	Generator() {}
+	~Generator() {}
+
+private:
+
+};
+
+
+int main()
+{
+	cout << "siema";
+}
---
+++ Projekt_PK/Projekt_PK/Main_Projektu.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <iostream>
+#include <cmath>
+#include <queue>
+
+using namespace std;
