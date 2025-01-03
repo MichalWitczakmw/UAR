87d00bfb2d95327c93d1070304c3f8af4d6c4cbb
MichalWitczakmw <mw305423@student.polsl.pl>
2025-01-03 21:08:14 +0100

    s

--- Projekt_PK/Projekt_PK/Main_Projektu.cpp
+++ Projekt_PK/Projekt_PK/Main_Projektu.cpp
@@ -3,59 +3,48 @@
 class Generator
 {
 public:
-	Generator(){}
-	~Generator(){}
+    Generator(){}
 
-	queue<double> Skokowo(int aktywacja, double wartoscStala)
-	{
-		for (int i = 0; i < wartoscZadana.size() ; i++)
-		{
-			if (terazniejszyCzasZegara > aktywacja)
-				wartoscZadana.push(wartoscStala);
-			else
-				wartoscZadana.push(0.0);
-			tik();
-		}
-		return wartoscZadana;
-	}
+    // Funkcja generuj¹ca wartoœæ skoku jednostkowego dla konkretnej chwili czasu
+    double Skokowo(int aktywacja, double wartoscStala, int i)
+    {
+        return (i >= aktywacja) ? wartoscStala : 0.0;
+    }
 
-	queue<double> Sinusoidalnie( double wartoscStala)
-	{
+    // Funkcja generuj¹ca wartoœæ sinusoidy dla konkretnej chwili czasu
+    double Sinusoidalnie(int okres, double amplituda, int i)
+    {
+        constexpr double m_PI = 3.14;
+        return amplituda * std::sin((i % okres) * 2 * m_PI / okres);
+    }
 
-		for (int i = 0; i < wartoscZadana.size(); i++)
-		{
-			wartoscZadana.push(sin((i % (int)(2*m_PI)/ (2 * m_PI))));
-		}
-		return wartoscZadana;
-	}
+    // Funkcja generuj¹ca wartoœæ sygna³u prostok¹tnego dla konkretnej chwili czasu
+    double SygnalProstokatny(int okres, double wypelnienie, double amplituda, int i)
+    {
+        return ((i % okres) < wypelnienie * okres) ? amplituda : 0.0;
+    }
 
-	queue<double> SygnalProstokatny(int aktywacja, double wartoscStala)
-	{
+    void AktualizujBufor(std::deque<double>& bufor, double nowaWartosc, int maxRozmiar)
+    {
+        if (bufor.size() == maxRozmiar)
+        {
+            bufor.pop_front();
+        }
+        bufor.push_back(nowaWartosc);
+    }
 
-		for (int i = 0; i < wartoscZadana.size(); i++)
-		{
-			wartoscZadana.push(sin((i % aktywacja) * 2 * m_PI / aktywacja));
-		}
-		return wartoscZadana;
-	}
 
-private:
-	queue<double> wartoscZadana = {};
-	const double m_PI = 3.14;
-	int terazniejszyCzasZegara = 0;
-
-	void tik()
-	{
-		this_thread::sleep_for(chrono::seconds(1));
-		terazniejszyCzasZegara++;
-	}
 };
+
+
 
 class Sprzerzenie
 {
 public:
 	Sprzerzenie() {}
 	~Sprzerzenie() {}
+
+
 
 private:
 
@@ -91,6 +80,61 @@ private:
 
 int main()
 {
-	cout << "siema";
+    int size = 10; // Rozmiar sygna³u (liczba próbek)
+    Generator generator;
+
+    // Parametry sygna³ów
+    int t = 5;              // chwila aktywacji
+    double A_step = 1.0;    // wartoœæ sta³a dla skoku
+    int T_sine = 6;         // okres dla sinusoidy
+    double A_sine = 1.0;    // amplituda sinusoidy
+    int T_square = 6;       // okres dla sygna³u prostok¹tnego
+    double p = 0.5;         // wype³nienie sygna³u prostok¹tnego
+    double A_square = 1.0;  // amplituda sygna³u prostok¹tnego
+
+    std::deque<double> stepSignal;
+    std::deque<double> sineSignal;
+    std::deque<double> squareSignal;
+
+  
+
+    int i = 0;
+    while (true)
+    {
+        // Generowanie nowych wartoœci sygna³ów
+        double stepValue = generator.Skokowo(t, A_step, i);
+        double sineValue = generator.Sinusoidalnie(T_sine, A_sine, i);
+        double squareValue = generator.SygnalProstokatny(T_square, p, A_square, i);
+
+        // Aktualizacja buforów
+        generator.AktualizujBufor(stepSignal, stepValue, size);
+        generator.AktualizujBufor(sineSignal, sineValue, size);
+        generator.AktualizujBufor(squareSignal, squareValue, size);
+
+        // Wyœwietlanie aktualnych wartoœci buforów
+        std::cout << "Czas: " << i << " sekund" << std::endl;
+        std::cout << "Bufor skoku jednostkowego: ";
+        for (double val : stepSignal)
+            std::cout << val << " ";
+        std::cout << std::endl;
+
+        std::cout << "Bufor sinusoidy: ";
+        for (double val : sineSignal)
+            std::cout << val << " ";
+        std::cout << std::endl;
+
+        std::cout << "Bufor sygna³u prostok¹tnego: ";
+        for (double val : squareSignal)
+            std::cout << val << " ";
+        std::cout << std::endl;
+
+        std::cout << std::endl;
+
+        // Czekanie 1 sekundy
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+
+        // Inkrementacja czasu
+        i++;
+    }
 	
 }
